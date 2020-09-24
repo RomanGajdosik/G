a4c29d9bc7ab6b01085f3d5e38c7774b2347fa6c
RomanGajdosik <rmgajdosik@gmail.com>
2020-09-24 23:17:20 +0200

    Error try funct

---
+++ blok.py
@@ -0,0 +1,6 @@
+s = input ('Zadaj cele cislo ')
+
+i=int(s)
+
+#vysl = i/5 
+print ('Zadane platne cele cislo ', i/5 )
--- practice.py
+++ practice.py
@@ -3,5 +3,7 @@ print('Hello world')
 import os
 
 dir = os.curdir 
+time=os.times()
 
 print(dir)
+print(time)
