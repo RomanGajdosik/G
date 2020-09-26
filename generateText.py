ecea4dd2820bb5de5f1cbffb2304148f7a5099d1
RomanGajdosik <rmgajdosik@gmail.com>
2020-09-26 17:03:32 +0200

    tuned

--- generateText.py
+++ generateText.py
@@ -2,16 +2,16 @@ import random
 count = 0 
 countE= input('Zadaj cele cislo od 1 do 10 , alebo ENTER pre Default = 5 ') 
 if countE:
-    countE = int(countE) -1 
+    countE = int(countE) 
 else:
-    countE = 4
-    
+    countE = 5
+
 zamena=["moj","tvoj","jeho","jej","nas",'vas']
 podstM=["muz",'Zena','pes',"macka",'Kon',"dom",'vlak']
 pridM=['tazky',"lahky","opacny",'Kratky',"Dlhy"]
 slovesa=['skace',"prdi","grga","otravuje"]
 
-while count <= countE :
+while count < countE :
     flag=random.randint(0,1)
     line = random.choice(zamena) + " "
     line += random.choice(podstM) + " "
